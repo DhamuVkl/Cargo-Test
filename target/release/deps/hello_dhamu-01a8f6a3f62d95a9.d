9a1daccd7ebe60da3997fa93b04a3b26fbfcce84
Dharagesh <sdhamuvkl@gmail.com>
2024-04-10 12:37:02 +0530

    release

---
+++ target/release/deps/hello_dhamu-01a8f6a3f62d95a9.d
@@ -0,0 +1,5 @@
+/home/jacky/RustProjects/hello_dhamu/target/release/deps/hello_dhamu-01a8f6a3f62d95a9: src/main.rs
+
+/home/jacky/RustProjects/hello_dhamu/target/release/deps/hello_dhamu-01a8f6a3f62d95a9.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/hello_dhamu.d
@@ -0,0 +1,1 @@
+/home/jacky/RustProjects/hello_dhamu/target/release/hello_dhamu: /home/jacky/RustProjects/hello_dhamu/src/main.rs
